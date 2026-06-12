045d25e5d2ff86fc4a071eab44a94aad69e761ec
BruceDai <feng.dai@intel.com>
2026-06-12 17:27:32 +0800

    save each failure subtest info into CSV file

--- src/main.js
+++ src/main.js
@@ -487,6 +487,7 @@ Examples:
 
                    // Store DLL info for this configuration
                    allDllResults.push({
+                      configName: config.name,
                       backend: config.name,
                       framework: fw,
                       device: config.device,
@@ -494,6 +495,7 @@ Examples:
                    });
 
                    runRes.forEach(r => {
+                       r.configName = config.name;
                        r.backend = config.name;
                        r.device = config.device;
                        r.fullConfig = config;
